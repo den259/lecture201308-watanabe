1d559e1f91c9759f736342b21cf80298bfeccb57
Jun Satou <j_sato@mse.biglobe.ne.jp>
2013-08-06 15:27:57 +0900

    Update Main001.rb

--- src/ruby/Subject008/Main001.rb
+++ src/ruby/Subject008/Main001.rb
@@ -1,4 +1,3 @@
-
 h ={"apple" =>100 ,"lemon"=>120 ,"orange"=>90 ,"grape"=>110}
 
 h["kiwi"]=150
@@ -11,3 +10,12 @@ puts h
 
 puts h.delete("apple" =>100 ,"lemon"=>120 ,"orange"=>90 ,"grape"=>110,"kiwi"=>150,"peach"=>200)
 
+# ↑ こうではなく、↓ 下を期待していました。すみません。誤解を生むような指示書で。
+
+h1 = { a: 'apple', o: 'orange' }
+
+puts h1
+
+h2 = { :a => 'apple', :o => 'orange' }
+
+puts h2
