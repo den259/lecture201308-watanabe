4b8ba8764fbb2c8ac257739df9120d7d6495720f
watanabe201308 <storm.w0125@gmail.com>
2013-08-19 17:13:19 +0900

    Ruby_on_Rails_2にコードを追加しました。（第４章まで）

---
+++ src/Ruby_on_Rails_2/work/hinagiku/db/migrate/20130809022942_create_tasks.rb
@@ -0,0 +1,17 @@
+class CreateTasks < ActiveRecord::Migration
+  def self.up
+    create_table :tasks do |t|
+     # t.integer :category_id
+      t.string :name
+      t.text :description
+      t.date :due_date
+      t.boolean :done
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :tasks
+  end
+end
