c26a240922f1990e7704e1cc4b58905275d83abc
watanabe201308 <storm.w0125@gmail.com>
2013-08-26 17:52:37 +0900

    Ruby_on_Rails_3にコードを追加しました。（第18章まで終わりました。）

--- src/Ruby_on_Rails_3/work/hinagiku/db/seeds/development/users.rb
+++ src/Ruby_on_Rails_3/work/hinagiku/db/seeds/development/users.rb
@@ -1,7 +1,10 @@
-%w(alice bob carol).each do |name|
+%w(alice bob carol).each_with_index do |name, index|
   user = User.new
   user.login_name = name
   user.display_name = name.capitalize
-  user.password = "opensesame!"
+  user.setting_password = true
+  user.password = "password"
   user.save!
+  email = Email.new(:address => user.login_name + "@example.com")
+  user.emails << email
 end
